21c97a2c8516b08128d8571783b57ecbc5c1b615
Gérzia Kelly <gerziakelly@hotmail.com>
2025-05-28 01:04:32 +0000

    nivel novato if

--- if2.c
+++ if2.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 
 int main() {
-  int nota = 65;
+  float temperatura , umidade;
+  unsigned int estoque ,  estoqueMinimo = 1000;
 
-  if (nota >= 60) {
-    printf("Você passou!\n");
+  // Variaveis 
+  prinf("Entre com a temperatura\n");
+  scanf("%f", &temperatura);
+  printf("Entre com a umidade \n");
+  scanf("%f", &umidade);
+  prinf("Entre com o estoque\n");
+  scanf("%u", &temperatura);
+
+  //Dados
+
+  if (temperatura > 30){
+    printf("Temperatura está alta\n");
   } else {
-    printf("Você não passou.\n");
+    printf("temperatura está dentro dos parametros\n");
   }
+    if (umidade > 50){
+     printf("Umidade elevada\n");
+  } else {
+    printf("Umidade está dentro dos parametros\n");
+  }
+    if (estoque < estoqueMinimo) {
+        printf("Estoque abaixo do mínimo!\n");
+    } else {
+        printf("Estoque normal!\n");
+    }
+    
 
-  return 0;
+
+
 }
